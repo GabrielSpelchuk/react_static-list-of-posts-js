65ac4caaec2ede994ea7ab647cb1035c91bfe97a
GabrielSpelchuk <gabrielspelciuc24@gmail.com>
2026-02-24 14:57:11 +0200

    add verify in UserInfo

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,7 +1,9 @@
 import './UserInfo.scss';
 
-export const UserInfo = ({ user }) => (
-  <a className="UserInfo" href={`mailto:${user.email}`}>
-    {user.name}
-  </a>
-);
+export const UserInfo = ({ user }) => {
+  return user ? (
+    <a className="UserInfo" href={`mailto:${user.email}`}>
+      {user.name}
+    </a>
+  ) : null;
+};
